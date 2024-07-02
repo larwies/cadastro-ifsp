03b8c27e16173246a30dd44d13cb1c3ec0569e9c
larwies <farquetelarissa@gmail.com>
2024-07-02 11:51:01 -0300

    a

---
+++ listarCidade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+        include('includes/conexao.php');
+        $sql = "select * from Cidade";
+        //executa a consulta 
+        $result = mysqli_query($con, $sql);
+        //retorna apenas uma linha 
+        
+    ?>
+
+    <h1>Consulta de cidades</h1>
+    <a href="cadastroCidade.html">Cadastrar nova cidade</a>
+    <table align="center" border="1" width="500">
+    <tr>
+        <th>Código</th>
+        <th>Nome</th>
+        <th>Estado</th>
+    </tr>
+    <?php
+        while($row = mysqli_fetch_array($result)){
+            echo "<tr>";
+            echo "<td>".$row['id']."</td>";
+            echo "<td>".$row['nome']."</td>";
+            echo "<td>".$row['estado']."</td>";
+            echo "<tr>";
+        }
+    ?>
+    </table>
+</body>
+</html>
